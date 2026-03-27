33d71a1f802d16f3eaeee82f14728d5d4a7b4c33
byte-padmanabhan <monkelo2004@gmail.com>
2026-03-27 20:24:17 +0530

    fix: the authenticate thing

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import  { getMe, login, logout, refreshToken, register } from "../controller/auth.controller.js";
+import authenticate from "../middleware/auth.middleware.js";
 const authRouter=Router();
 authRouter.post("/register",register)
 authRouter.post("/login",login)
-authRouter.get("/me",getMe)
+authRouter.get("/me",authenticate,getMe)
 authRouter.post('/refresh-token',refreshToken); 
-authRouter.post('/logout',logout);
+authRouter.post('/logout',authenticate,logout);
 export default authRouter;
